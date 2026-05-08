2539516da659a939dcd2bc4b5d88bb58ff4274ea
Shaikh Abdulrauf Asifparvez <arauf0801@gmail.com>
2026-05-08 09:57:28 +0530

    critical push 2

--- build.sh
+++ build.sh
@@ -5,10 +5,7 @@ pip install -r requirements.txt
 
 npm install
 
+# Set PATH so collectstatic can find the node executable
+export PATH="/opt/render/project/src/node_modules/.bin:$PATH"
+
 python manage.py collectstatic --no-input
-
-# This ensures your Django project can find the node command in the Cloud Run environment
-import os
-os.environ['PATH'] = "/workspace/node_modules/.bin:/workspace:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-
-# Then, when collectstatic runs, it will find the node executable.
