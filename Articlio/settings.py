bccd507fdfb4f50cecf3932abc72e62445b335dc
Shaikh Abdulrauf Asifparvez <arauf0801@gmail.com>
2026-09-18 17:50:37 +0530

    Remove cloudinary_storage from INSTALLED_APPS to fix broken static file error in collectstatic

--- Articlio/settings.py
+++ Articlio/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django.contrib.sitemaps',
